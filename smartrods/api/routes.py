bbdf742a9428f12b5363c07e3ab284fc017d7dab
pierreazalbert <p.azalbert@bboxx.co.uk>
2017-05-19 11:53:49 +0100

    updated API

--- smartrods/api/routes.py
+++ smartrods/api/routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, abort, make_response, jsonify
+from flask import Blueprint, abort, make_response, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from flask_restful import Api, Resource
+from smartrods import db
 from smartrods.models import Board, Rods
 
 mod = Blueprint('api', __name__)    # Initialise Blueprint for API
@@ -23,7 +24,10 @@ def unauthorized():
 #     return '{"result":"You are in the API!"}'
 
 class BoardAPI(Resource):
+
     decorators = [auth.login_required]
+
+    # Get information about a specific board
     def get(self, id):
         board = Board.query.get(id)
         if board is None:
@@ -32,7 +36,38 @@ class BoardAPI(Resource):
                 'is_connected':board.is_connected,
                 'user':board.user.firstname+' '+board.user.lastname,
                 'last_update':str(board.rods[-1].timestamp),
-                'contents':board.rods[-1].rods}
+                'rods':board.rods[-1].rods}, 200
+
+    # Update rods on board
+    def post(self, id):
+
+        # Check board exists
+        board = Board.query.get(id) #check existence instead of loading object?
+        if board is None:
+            return {'error': 'There is no board with the requested ID'}, 404
+
+        # Check correct format of request
+        if not request.json or not 'timestamp' in request.json or not 'rods' in request.json:
+            return {'error': 'The request format is invalid. You must supply a timestamp and a rods object'}, 404
+
+        # Check correct format of request arguments (rods and timestamp)
+        if type(request.json['timestamp']) != unicode:
+            return {'error': 'Please provide a valid timestamp string (YYYY-MM-DD hh:mm:ss)'}, 404
+        if type(request.json['rods']) is not list or len(request.json['rods']) is not 10:
+            return {'error': 'Please provide a valid rods data structure (10*10 2D array with values of type Int between 0 and 10).'}, 404
+        for i in request.json['rods']:
+            if type(i) is not list or len(i) is not 10:
+                return {'error': 'Please provide a valid rods data structure (10*10 2D array with values of type Int between 0 and 10).'}, 404
+
+        # Create new rods object
+        newrods = Rods(timestamp=request.json['timestamp'], rods=request.json['rods'], board_id=id)
+        db.session.add(newrods)
+        db.session.commit()
+        return {'timestamp':request.json['timestamp'],
+                'rods':request.json['rods'],
+                'board_id':id}, 201
+
+        #     if
 
     # def post(self):
     #     if not request.json or not 'id' in request.json or not 'rods' in request.json:
